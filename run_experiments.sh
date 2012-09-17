6740515bbc05a2c49a300f525ed7ccc70df38d1a
Dan Holcomb <holcomb@eecs.berkeley.edu>
2012-09-17 12:05:13 -0700

    fixed bug in script

--- run_experiments.sh
+++ run_experiments.sh
@@ -4,14 +4,6 @@
 make dump.out
 
 
-# find_t_cex.pl --network ex_tree1 --t_min 6 --t_max 30 --bmc_timeout 30 --bmc_frames 60
-# compare_pdr_vs_kind.pl --network ex_tree1 --timeout 100 --log_dir log_ex_queue
-
-# find_t_cex.pl --network ex_tree2 --t_min 6 --t_max 30 --bmc_timeout 30 --bmc_frames 60
-# compare_pdr_vs_kind.pl --network ex_tree2 --timeout 100 --log_dir log_ex_queue
-
-
-
 
 find_t_cex.pl --network ex_queue_chain1 --t_min 1 --t_max 30 --bmc_timeout 300 --bmc_frames 100
 compare_pdr_vs_kind.pl --network ex_queue_chain1 --timeout 600 --log_dir log_ex_queue_chain1
@@ -29,16 +21,16 @@ find_t_cex.pl --network ex_queue_chain5 --t_min 5 --t_max 100 --bmc_timeout 300 
 compare_pdr_vs_kind.pl --network ex_queue_chain5 --timeout 600 --log_dir log_ex_queue_chain5
 
 find_t_cex.pl --network ex_queue_chain6 --t_min 5 --t_max 100 --bmc_timeout 300 --bmc_frames 100
-compare_pdr_vs_kind.pl --network ex_queue_chain5 --timeout 600 --log_dir log_ex_queue_chain6
+compare_pdr_vs_kind.pl --network ex_queue_chain6 --timeout 600 --log_dir log_ex_queue_chain6
 
 find_t_cex.pl --network ex_queue_chain7 --t_min 5 --t_max 100 --bmc_timeout 300 --bmc_frames 100
-compare_pdr_vs_kind.pl --network ex_queue_chain5 --timeout 600 --log_dir log_ex_queue_chain7
+compare_pdr_vs_kind.pl --network ex_queue_chain7 --timeout 600 --log_dir log_ex_queue_chain7
 
 find_t_cex.pl --network ex_queue_chain8 --t_min 5 --t_max 100 --bmc_timeout 300 --bmc_frames 100
-compare_pdr_vs_kind.pl --network ex_queue_chain5 --timeout 600 --log_dir log_ex_queue_chain8
+compare_pdr_vs_kind.pl --network ex_queue_chain8 --timeout 600 --log_dir log_ex_queue_chain8
 
 find_t_cex.pl --network ex_queue_chain9 --t_min 5 --t_max 100 --bmc_timeout 300 --bmc_frames 100
-compare_pdr_vs_kind.pl --network ex_queue_chain5 --timeout 600 --log_dir log_ex_queue_chain9
+compare_pdr_vs_kind.pl --network ex_queue_chain9 --timeout 600 --log_dir log_ex_queue_chain9
 
 
 
@@ -59,16 +51,16 @@ find_t_cex.pl --network ex_queue5 --t_min 5 --t_max 100 --bmc_timeout 300 --bmc_
 compare_pdr_vs_kind.pl --network ex_queue5 --timeout 600 --log_dir log_ex_queue5
 
 find_t_cex.pl --network ex_queue6 --t_min 5 --t_max 100 --bmc_timeout 300 --bmc_frames 100
-compare_pdr_vs_kind.pl --network ex_queue5 --timeout 600 --log_dir log_ex_queue6
+compare_pdr_vs_kind.pl --network ex_queue6 --timeout 600 --log_dir log_ex_queue6
 
 find_t_cex.pl --network ex_queue7 --t_min 5 --t_max 100 --bmc_timeout 300 --bmc_frames 100
-compare_pdr_vs_kind.pl --network ex_queue5 --timeout 600 --log_dir log_ex_queue7
+compare_pdr_vs_kind.pl --network ex_queue7 --timeout 600 --log_dir log_ex_queue7
 
 find_t_cex.pl --network ex_queue8 --t_min 5 --t_max 100 --bmc_timeout 300 --bmc_frames 100
-compare_pdr_vs_kind.pl --network ex_queue5 --timeout 600 --log_dir log_ex_queue8
+compare_pdr_vs_kind.pl --network ex_queue8 --timeout 600 --log_dir log_ex_queue8
 
 find_t_cex.pl --network ex_queue9 --t_min 5 --t_max 100 --bmc_timeout 300 --bmc_frames 100
-compare_pdr_vs_kind.pl --network ex_queue5 --timeout 600 --log_dir log_ex_queue9
+compare_pdr_vs_kind.pl --network ex_queue9 --timeout 600 --log_dir log_ex_queue9
 
 
 
@@ -85,16 +77,16 @@ find_t_cex.pl --network ex_tree5 --t_min 5 --t_max 100 --bmc_timeout 300 --bmc_f
 compare_pdr_vs_kind.pl --network ex_tree5 --timeout 600 --log_dir log_ex_tree5
 
 find_t_cex.pl --network ex_tree6 --t_min 5 --t_max 100 --bmc_timeout 300 --bmc_frames 100
-compare_pdr_vs_kind.pl --network ex_tree5 --timeout 600 --log_dir log_ex_tree6
+compare_pdr_vs_kind.pl --network ex_tree6 --timeout 600 --log_dir log_ex_tree6
 
 find_t_cex.pl --network ex_tree7 --t_min 5 --t_max 100 --bmc_timeout 300 --bmc_frames 100
-compare_pdr_vs_kind.pl --network ex_tree5 --timeout 600 --log_dir log_ex_tree7
+compare_pdr_vs_kind.pl --network ex_tree7 --timeout 600 --log_dir log_ex_tree7
 
 find_t_cex.pl --network ex_tree8 --t_min 5 --t_max 100 --bmc_timeout 300 --bmc_frames 100
-compare_pdr_vs_kind.pl --network ex_tree5 --timeout 600 --log_dir log_ex_tree8
+compare_pdr_vs_kind.pl --network ex_tree8 --timeout 600 --log_dir log_ex_tree8
 
 find_t_cex.pl --network ex_tree9 --t_min 5 --t_max 100 --bmc_timeout 300 --bmc_frames 100
-compare_pdr_vs_kind.pl --network ex_tree5 --timeout 600 --log_dir log_ex_tree9
+compare_pdr_vs_kind.pl --network ex_tree9 --timeout 600 --log_dir log_ex_tree9
 
 
 
