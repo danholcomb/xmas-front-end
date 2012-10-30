e95f6fc168e22bad7deb1d9ed09688c2a3c2fd85
Dan Holcomb <holcomb@eecs.berkeley.edu>
2012-10-29 21:55:21 -0700

    map invariant cmd line args to tex strings instead of storing redundantly

--- scripts/compare_pdr_vs_kind.pl
+++ scripts/compare_pdr_vs_kind.pl
@@ -4,10 +4,10 @@ use Carp;
 use strict;
 use Data::Dumper::Simple;
 $Data::Dumper::Sortkeys = 1;
+use Getopt::Long;
 
 require "/Users/dan/xmas_front_end/xmas-front-end/scripts/abc_utils.pl";
 
-use Getopt::Long;
 
 my $Network = "ex_queue";
 my $Timeout = 20;
@@ -34,59 +34,42 @@ print "\n";
 my $Timestamp = `date`;
 my $Experiments = ();
 
-#or ($Network eq "credit_loop_numinv")
 if (($Network eq "credit_loop")) {
      push (@$Experiments, { "ENGINE" => "pdr" ,        
-			    "PROP_DEF" => " --enable_phig --enable_persistance --enable_psi --t_max $T_Max",
-			    "PROP_STRING" => '$\Phi^G$ \wedge \Psi' });
+			    "PROP_DEF" => " --enable_phig --enable_persistance --enable_psi --t_max $T_Max"});
 
      push (@$Experiments, { "ENGINE" => "pdr" ,        
-			    "PROP_DEF" => " --enable_phig --enable_persistance --enable_psi --enable_num_inv --t_max $T_Max",
-			    "PROP_STRING" => '$\Phi^G \wedge \Psi \wedge Num$' });
+			    "PROP_DEF" => " --enable_phig --enable_persistance --enable_psi --enable_num_inv --t_max $T_Max" });
 
      push (@$Experiments, { "ENGINE" => "kind" ,        
-			    "PROP_DEF" => " --enable_phig --enable_persistance --enable_psi --t_max $T_Max",
-			    "PROP_STRING" => '$\Phi^G \wedge \Psi$' });
+			    "PROP_DEF" => " --enable_phig --enable_persistance --enable_psi --t_max $T_Max" });
 
      push (@$Experiments, { "ENGINE" => "kind" ,        
-			    "PROP_DEF" => " --enable_phig --enable_persistance --enable_psi --enable_num_inv --t_max $T_Max",
-			    "PROP_STRING" => '$\Phi^G \wedge \Psi \wedge Num$' });
+			    "PROP_DEF" => " --enable_phig --enable_persistance --enable_psi --enable_num_inv --t_max $T_Max" });
 
 } else {
      # these settings assume that propagation succeeds, and will cause problems otherwise
      # each experiment is one row of the table
      push (@$Experiments, { "ENGINE" => "pdr" ,        
-			    "PROP_DEF" => " --enable_phig",
-			    "PROP_STRING" => '$\Phi^G$' });
+			    "PROP_DEF" => " --enable_phig" });
 
      push (@$Experiments, { "ENGINE" => "pdr" ,        
-			    "PROP_DEF" => " --enable_phig --enable_psi",
-			    "PROP_STRING" => '$\Psi \wedge \Phi^G$' });
+			    "PROP_DEF" => " --enable_phig --enable_psi" });
 
      push (@$Experiments, { "ENGINE" => "pdr" ,        
-			    "PROP_DEF" => " --enable_lemmas --enable_phig --enable_psi",
-			    "PROP_STRING" => '$\Phi^L \wedge \Psi \wedge \Phi^G$' });
+			    "PROP_DEF" => " --enable_lemmas --enable_phig --enable_psi" });
 
      push (@$Experiments, { "ENGINE" => "pdr" ,        
-			    "PROP_DEF" => " --enable_lemmas --enable_phig --enable_psi --enable_bound_channel --enable_response_bound_channel",
-			    "PROP_STRING" => '$\Phi^L \wedge \Psi \wedge \Phi^G$ \wedge Channel' });
-
-
-     # push (@$Experiments, { "ENGINE" => "kind" ,        
-     # 		       "PROP_DEF" => " --enable_phig",
-     # 		       "PROP_STRING" => '$\Phi^G$' });
+			    "PROP_DEF" => " --enable_lemmas --enable_phig --enable_psi --enable_bound_channel --enable_response_bound_channel" });
 
      push (@$Experiments, { "ENGINE" => "kind" ,        
-			    "PROP_DEF" => " --enable_psi --enable_phig",
-			    "PROP_STRING" => '$\Psi \wedge \Phi^G$' });
+			    "PROP_DEF" => " --enable_psi --enable_phig" });
 
      push (@$Experiments, { "ENGINE" => "kind" ,        
-			    "PROP_DEF" => " --enable_lemmas --enable_phig --enable_psi",
-			    "PROP_STRING" => '$\Phi^L \wedge \Psi \wedge \Phi^G$' });
+			    "PROP_DEF" => " --enable_lemmas --enable_phig --enable_psi" });
 
      push (@$Experiments, { "ENGINE" => "kind" ,        
-			    "PROP_DEF" => " --enable_lemmas --enable_phig --enable_psi --enable_bound_channel --enable_response_bound_channel",
-			    "PROP_STRING" => '$\Phi^L \wedge \Psi \wedge \Phi^G$ \wedge Channel' });
+			    "PROP_DEF" => " --enable_lemmas --enable_phig --enable_psi --enable_bound_channel --enable_response_bound_channel" });
 
 }
 print Dumper($Experiments);
@@ -95,9 +78,7 @@ print Dumper($Experiments);
 # generate result for each experiment
 my $R = ();
 foreach my $i (0..$#{$Experiments}) {
-
      my $prop_def     = $Experiments->[$i]->{PROP_DEF};
-     my $prop_string  = $Experiments->[$i]->{PROP_STRING};
      my $engine       = $Experiments->[$i]->{ENGINE};
 
      system('./dump.out --network '.$Network.' '.$prop_def.' --dump dump.v');
@@ -110,26 +91,20 @@ foreach my $i (0..$#{$Experiments}) {
 	  $r->{RUN_TIME} = $Timeout;
      }
      $r->{ENGINE} = "$engine";
-     $r->{PROP_STRING} = "$prop_string";
      $r->{PROP_DEF} = "$prop_def";
      $r->{PROVED} = ($r->{VALID} == 1) ? "Y" : "-";
 
      my $log_file_name = 'compare_engines__'.$Network.'_'.$i.'' ;
      $r->{LOG_FILE_NAME} = $log_file_name;
-
      push (@$R,$r);
-     #print print_table($R);
-     
+
      writeFile("current_results.txt",print_table($R)); # just to check on progress
      appendFile("current_results.txt",Dumper($R));
 
-     #system('mkdir -p '.$Log_Dir."/".$log_file_name.'');
      system('mkdir -p '.$log_file_name.'');
      system('cp abc.rc dump.abc dump.aig dump.v current_results.txt '.$log_file_name.'/');
-     #     system('tar -czf '.$Log_Dir.'/'.$log_file_name.'.tar.gz '.$log_file_name.'/');
      system('tar -czf '.$Log_Dir.'/'.$log_file_name.'.tar.gz '.$log_file_name.'/');
      system('rm -r '.$log_file_name.'');
-     #croak;
 }
 
 appendFile("exp_results.txt",print_table($R));
@@ -138,6 +113,17 @@ print Dumper($R);
 exit;
 
 
+sub propdef_to_propstring {
+     my $d = shift;
+     my @symbols = ();
+     push (@symbols, '\Phi^L')  if ($d =~ m/--enable_lemmas/);
+     push (@symbols, '\Phi^G')  if ($d =~ m/--enable_phig/);
+     push (@symbols, '\Psi')    if ($d =~ m/--enable_psi/);
+     push (@symbols, 'Num')    if ($d =~ m/--enable_num_inv/);
+     push (@symbols, 'Channel') if ($d =~ m/--enable_bound_channel/ and $d =~ m/enable_response_bound_channel/);
+     my $s = "\$".join(' \wedge ',@symbols)."\$";
+     return $s;
+}
 
 
 sub print_table {
@@ -158,10 +144,9 @@ sub print_table {
 			 $r->[$i]->{FRAMES},
 			 $r->[$i]->{PROVED},
 			 $r->[$i]->{ENGINE},
-			 $r->[$i]->{PROP_STRING},
+			 propdef_to_propstring($r->[$i]->{PROP_DEF}),
 			);
      }
      return $t;
-
 }
 
